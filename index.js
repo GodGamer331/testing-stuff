220a3f3b11dae0c21b4e3a2d051f74420020123b
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-13 22:55:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const config = require("./botconfig.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true})
 
+bot.on("ready", async() => {
+  console.log("Bot je ready!")
+  bot.user.setActivity("Codes in commands!", (type: "STREAMING"))
+})
+
+
+bot.login("config.token");
