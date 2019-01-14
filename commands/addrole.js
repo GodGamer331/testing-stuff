89d1a1cdf49dea3a9c6677fb291a329b0cc30e9e
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-14 07:16:55 +0100

    Create addrole.js

---
+++ commands/addrole.js
@@ -0,0 +1,24 @@
+const discord = require("diecord.js");
+
+modules.exports.run = async (bot, message, args) => {
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You dont have enought permissions!");
+  let aMember = message.guild.member(message.mentions.user.first()) || message.guild.get(args[0]);
+  if(!aMember) return message.reply("Specify a Member!");
+  let aRole = args.join(" ").slice(22);
+  if(!aRole) return message.reply("Please specify role aswell.");
+  let gRole = message.guild.role.find("name", role);
+  if(!gRole) return message.reply("I couldnt find that role!");
+  if(aMember.roles.has(aRole.id));
+  await(aMember.addRole(aRole.id));
+  
+  try{
+    aMember.send("You now have role: " + `${gRole.name}`);
+  }catch(e){
+  message.channel.send(`User have now ${gRole.name}. I tried to DM him/her but his/her DM's are locked.`);
+}
+
+
+module.exports.help = {
+  name: "addrole",
+  aliases: ["arole", "roleadd", "newrole"]
+}
