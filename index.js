03c08c748d92e532d015708f6355c3671a5d732b
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-14 02:22:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const config = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true})
+const fs = require("fs");
 
 bot.on("ready", async() => {
   console.log("Bot je ready!")
   bot.user.setActivity("Codes in commands!", {type: "STREAMING"})
 })
 
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("/commands/", (err, files) => {
+
+  if(err) console.log(err)
+  
+  let jsfile = files.filter(f => f.split(".").pop === "js")
+  if(jsfile.lenght <= 0) {
+     return console.log("[LOGS] Couldnt find command!");
+     
+     }
+  
+   jsfile.forEach((f, i) => {
+       let pull = require(`./commands/${f}`)
+       bot.commands.set(pull.config.name, pull);
+       pull.config.aliases1.forEach(alias, => {
+           bot.aliases.set(alias, pull.config.name)
+    
+      });
+   });
+});
 
 bot.login(process.env.token);
