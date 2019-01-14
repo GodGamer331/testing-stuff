6950f232e34a885daa2e2f3416c7c2c8b0634059
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-14 02:34:54 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+   let embed = new Discord.RichEmbed()
+   .setAuthor("Help Message", ${message.author.avatarURL})
+   .setColor("GREEN")
+   .setTimestamp()
+   .addBlankField("Test)
+   message.channel.send(embed)
+}
+
+module.export.config = {
+  name: "help",
+  aliases: ["helo", "hrlp"]
+}
