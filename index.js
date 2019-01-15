94d057572e42cc4b9142094dcacec21dd06293ba
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-15 03:47:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,30 +15,24 @@ bot.on("ready", async () => {
   bot.user.setActivity("All codes in commands.", {type: "STREAMING"});
 });
 
-const fs = require("fs");
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
-
 fs.readdir("./commands/", (err, files) => {
 
-    if(err) console.log(err)
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
 
-    let jsfile = files.filter(f => f.split(".").pop() === "js") 
-    if(jsfile.length <= 0) {
-         return console.log("[LOGS] Couldn't Find Commands!");
-    }
-
-    jsfile.forEach((f, i) => {
-      let pull = require(`./commands/${f}`);
-      
-      bot.commands.set(pull.config.name, pull);  
-      pull.config.aliases.forEach(alias => {
-          bot.aliases.set(alias, pull.config.name)
-      });
-   });
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
 });
 
-//wutf D:
+
+
 
 bot.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
