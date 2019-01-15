f0cd5d00cc0dc953ec9b49d1c186a3feff82c17e
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-15 03:49:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const botconfig = require("./botconfig.json");
+const fs = require("fs");
 //const colours = require("./colours.json");
 //const superagent = require("superagent")
 //const ms = require("ms")
