e06d31b1ccb314a8a078a96854d551f555623ecd
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-15 04:34:06 +0100

    Create T1Loading.js

---
+++ commands/T1Loading.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+   message.channel.send(":T1Loading:")
+}
+
+module.exports.help = {
+  name: "loading",
+  aliases: []
+}
