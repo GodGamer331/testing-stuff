b2133310ff6a6f1571af614be83da4fb0a286d8c
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-16 18:14:19 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,14 +3,14 @@ const Discord = require("discord.js");
 //const ms = require("ms")
 
 module.exports.run = async (bot, message, args) => {
-   let replis = ["Yes", "No", "Probably Yes", "Probably No", "Maybe", "Ask another one.", "Try asking later."]
+   let replies = ["Yes", "No", "Probably Yes", "Probably No", "Maybe", "Ask another one.", "Try asking later."]
    if(!args[2]) return message.reply("Ask a question that have 2 words!");
-   let answ = Math.floor((Math.random() * replis.lenght));
+   let answ = Math.floor((Math.random() * replies.lenght));
    let Q = args.slice(0).join(" ");
    
    var embed = new Discord.RichEmbed()
    .setAuthor(message.author.username)
-   .setDescription("Your Question was:" + Q + "\n My answer is" + answ);
+   .setDescription("Your Question was: " + Q + "\n My answer is " + answ);
   
    message.channel.send(embed)
 }
