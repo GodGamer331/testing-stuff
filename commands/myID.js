1028d2c4ed5b61baf2c30aba124fa3ff4308779f
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-16 15:25:11 +0100

    Create myID.js

---
+++ commands/myID.js
@@ -0,0 +1,16 @@
+const Botconfig = require("../botconfig.json");
+const Discord = require("discord.js");
+//const ms = require("ms")
+
+module.exports.run = async (bot, message, args) => {
+   var embed = new Discord.RichEmbed()
+   .setThumbnail(message.author.avatarURL)
+   .setDescription(`Your ID is ${message.author.id}`)
+   .setAuthor(message.author.username);
+   message.channel.send(embed)
+}
+
+module.exports.help = {
+    name: "my id",
+    aliases: []
+}
