eb8173fbfd345f44c2e48e0a59c2085978acb333
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-17 15:24:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ bot.on("message", async message => {
     let args = messageArray.slice(1);
     let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
     if(!prefixes[message.guild.id]){
+      console.log(prefix)
       prefixes[message.guild.id] = {
         prefixes: botconfig.prefix
       }
