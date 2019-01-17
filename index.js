5db67391e08f5b16f7f738fa8b82f3fa498bbf76
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-17 15:12:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
 
-    let prefix = botconfig.prefix;
+    //let prefix = botconfig.prefix;
     let messageArray = message.content.split(" ")
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
