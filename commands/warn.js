9363780fd60c9c3b05fd96185ad6ef1cfefc5f36
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-25 20:41:47 +0100

    Create warn.js

---
+++ commands/warn.js
@@ -0,0 +1,24 @@
+const Botconfig = require("../botconfig.json");
+const Discord = require("discord.js");
+//const ms = require("ms")
+
+module.exports.run = async (bot, message, args) => {
+   let staff = message.member.roles.find("name", "Moderator") || message.member.roles.find("name", "Administrator") || message.member.roles.find("name", "Co-Owner") || message.member.roles.find("name", "Owner");
+   if(!staff) return message.reply("You are not a Staff Member!");
+   
+   .setAuthor("Warn Announce")
+   .setColor("RED")
+   .setTimestamp()
+   .addField("Warned User:", `${message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])}`)
+   .addField("Moderator:", `${message.author.username}`)
+   .addField("Reason:", `${args.join(" ").slice(22)}`);
+   let wchannel = message.guild.channels.find(`name`, "logs");
+   wchannel.send(embed)
+   if(!wchannel) return message.reply("Pleaae setup a ``#logs`` channel.");
+   
+}
+
+module.exports.help = {
+    name: "warn",
+    aliases: []
+}
