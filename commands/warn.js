94604e13db8a1538e77b15789b0758512df4c781
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-25 20:52:44 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -6,14 +6,18 @@ module.exports.run = async (bot, message, args) => {
    //
    let staff = message.member.roles.find("name", "Moderator") || message.member.roles.find("name", "Administrator") || message.member.roles.find("name", "Co-Owner") || message.member.roles.find("name", "Owner");
    if(!staff) return message.reply("You are not a Staff Member!");
-   
+   let reason = args.join(" ").slice(22);
+   if(!reason) return message.reply("Please specify a reason!");
+   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]));
+   if(!wUser) return message.reply("Specify A User!");
+  
    var embed = new Discord.RichEmbed()
    .setAuthor("Warn Announce")
    .setColor("RED")
    .setTimestamp()
-   .addField("Warned User:", `${message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])}`)
-   .addField("Moderator:", `${message.author.username}`)
-   .addField("Reason:", `${args.join(" ").slice(22)}`);
+   .addField("Warned User:", wUser)
+   .addField("Moderator:", message.author.username)
+   .addField("Reason:", reason);
    let wchannel = message.guild.channels.find(`name`, "logs");
    wchannel.send(embed)
    if(!wchannel) return message.reply("Pleaae setup a ``#logs`` channel.");
