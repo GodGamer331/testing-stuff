83f9a7591a133ed8fb8e16813f8503e0847c6995
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-25 09:17:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const botconfig = require("./botconfig.json");
-const fs = require("fs");
+//const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 //bot.commands = new Discord.Collection();
 //bot.aliases = new Discord.Collection();
