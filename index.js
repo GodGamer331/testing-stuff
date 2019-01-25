b88b4d1d8111ef5510aea8817b809262f8063bfe
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-25 20:26:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
+//const tokenfile = require("./token.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
