c398bb5ac0249bcfa3390bee1ff7f2e956855c3f
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-26 17:11:28 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
 const PREFIX = "!="
+const superagent = require('superagent');
 
 module.exports.run = async (bot, message, args) => {
     if (!message.content.startsWith(PREFIX)) return;
