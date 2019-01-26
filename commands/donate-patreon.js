d087529d92e104890c4e0ced6bea74fe0b1a7382
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-26 14:51:12 +0100

    Update donate-patreon.js

--- commands/donate-patreon.js
+++ commands/donate-patreon.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
    var embed = new Discord.RichEmbed()
-   .setAuthor("Donate me tought here", https://cdn.discordapp.com/avatars/216303189073461248/00a6db63b09480d1613877bf40e98bea.png?size=2048)
+   .setAuthor("Donate me tought here", "https://cdn.discordapp.com/avatars/216303189073461248/00a6db63b09480d1613877bf40e98bea.png?size=2048")
    .setColor("0xFFFF00")
    .addField("Donate tought Patreon?", "<:GWgodbotsupportPatreon:538702233714294814> [Patreon here](https://www.patreon.com/user?u=16811839)")
    .addField("Please Note:", "If you are not in Support server then screen that you bought that. or I wont belive you.")
