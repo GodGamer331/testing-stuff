c6e13ceecd7fb502df11d3e634af076ef450aad7
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-26 14:30:38 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,6 @@ module.exports.run = async (bot, message, args) => {
   var embed = new Discord.RichEmbed()
   .setAuthor("Bot made by: JustNela#6666")
   .setThumbnail(message.author.avatarURL)
-  .setDescription("<:GWtestDiscord:533890341103730698> [Support Server](https://discord.gg/4DAZqsR) \n <:GWtestOnline:533904223872745482> [Invite me by clicking here](https://discordapp.com/api/oauth2/authorize?client_id=534122074579664896&permissions=8&scope=bot)")
   .addField("Ping!", ":ping_pong: **Pong!**");
   message.channel.send(embed)
 }
