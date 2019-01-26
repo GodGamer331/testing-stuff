f0f4f13e3aaad1e99430992d4ece1846a1532e5c
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-26 14:30:26 +0100

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -5,7 +5,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
    var embed = new Discord.RichEmbed()
    .setAuthor("Support!", message.author.avatarURL)
-   .addField("Discord:", "[Invite](https://discord.gg/4DAZqsR)")
+   .addField("Discord:", "<:GWgodbotsupportDiscord:538702233634734090> [Invite](https://discord.gg/4DAZqsR)")
+   .addField("Invite me:", "<:GWtestOnline:533904223872745482> [here](https://discordapp.com/api/oauth2/authorize?client_id=534122074579664896&permissions=8&scope=bot)")
+   
    .setColor("0xf4eb42")
    .setThumbnail(message.author.avatatURL);
    message.channel.send(embed)
