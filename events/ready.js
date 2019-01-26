4d23328ad260223fe93a629fe9c253831785eab5
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-26 21:38:49 +0100

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+
+
+module.exports = bot => {
+     console.log(`${bot.user.username} is online`)
+    // bot.user.setActivity("Hello", {type: "STREAMING", url:"https://twitch.tv/Strandable"});
+
+    let statuses = [
+        `to ${bot.guilds.size} servers!`,
+        "my prefix is !="
+        "start with !=help",
+        `over ${bot.users.size} users!`
+    ]
+
+    setInterval(function() {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        bot.user.setActivity(status, {type: "LISTENING"});
+
+    }, 5000)
+
+}
