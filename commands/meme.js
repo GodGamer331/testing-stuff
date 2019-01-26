315953b5c807976d4d9b87b5664f250597b325e5
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-26 17:47:44 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -3,36 +3,19 @@ const PREFIX = "!="
 const superagent = require('superagent');
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.content.startsWith(PREFIX)) return;
-
-    let msg = await message.channel.send("Generating...")
-
+    
     let {body} = await superagent
-    .get(`https://api-to.get-a.life/meme`);
-    if(!{body}) return message.channel.send("I broke! Try again.")
-
-    message.channel.send({embed: {
-        color: 3447003,
-        image: {
-            url: (body.url)
-          },
-          timestamp: new Date(),
-          footer: {
-          icon_url: bot.user.displayAvatarURL ,
-          text: "© StrandBot",
-        },
-        author: {
-            icon_url: message.guild.iconURL,
-            name: "Strand's Public Discord",
-          }
-        }});
-
-        msg.delete();
-      } 
-
-module.exports.conf = {
-    aliases: ['memes']
-};
+    .get('https://api-to.get-a.life/meme');
+    
+    var embed = new Discord.RichEmbed()
+    .setAuthor("MEME!!")
+    .setImage(body.url);
+    
+    message.channel.send(embed)
+    
+//module.exports.conf = {
+ //   aliases: ['memes']
+//};
 
 // ADD DESCRIPTION AND SUCH
 module.exports.help = {
