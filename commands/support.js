0bc39914e39ab72a833368b3448e10c03adb9326
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-26 13:40:32 +0100

    Create support.js

---
+++ commands/support.js
@@ -0,0 +1,17 @@
+const Botconfig = require("../botconfig.json");
+const Discord = require("discord.js");
+//const ms = require("ms")
+
+module.exports.run = async (bot, message, args) => {
+   var embed = new Discord.RichEmbed()
+   .setAuthor("Support!", message.author.avatarURL)
+   .addField("Discord:", "[Invite](https://discord.gg/4DAZqsR)")
+   .setColor(22023768)
+   .setThumbnail(message.author.avatatURL);
+   message.channel.send(embed)
+}
+
+module.exports.help = {
+    name: "support",
+    aliases: []
+}
