aac9da20a2b390f1f2319f45427d22776d6c34fa
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-27 21:08:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const moment = require("moment");
 const db = require("quick.db");
+const ms = require("ms");
 
 //const superagent = require("superagent")
 
@@ -79,7 +80,31 @@ bot.on("message", async message => {
 
 
 };
-    
+ let timeout = 86400000 // 24 hours in milliseconds, change if you'd like.
+    let amount = 500
+    // random amount: Math.floor(Math.random() * 1000) + 1;
+
+
+    let daily = await db.fetch(`daily_${message.author.id}`);
+
+    if (daily !== null && timeout - (Date.now() - daily) > 0) {
+        let time = ms(timeout - (Date.now() - daily));
+
+        message.channel.send(`You already collected ur daily reward, you can come back and collect it in **${time.hours}h ${time.minutes}m ${time.seconds}s**!`)
+    } else {
+    let embed = new Discord.RichEmbed()
+    .setAuthor(`Daily`, message.author.displayAvatarURL)
+    .setColor("GREEN")
+    .setDescription(`**Daily Reward**`)
+    .addField(`Collected`, amount)
+
+    message.channel.send(embed)
+    db.add(`money_${message.author.id}`, amount)
+    db.set(`daily_${message.author.id}`, Date.now())
+        
+    }
+
+};
 
   
  //WUTF IS DAT THING
