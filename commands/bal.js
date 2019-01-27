4aad6c9b29abebd8497bf46c3cdef34b18b8d23f
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-27 16:48:47 +0100

    Create bal.js

---
+++ commands/bal.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+module.exports.run = async (bot, message, args) => {
+
+    let bal = db.fetch(`money_${message.guild.id}_${message.author.id}`)
+
+    if (bal === null) bal = 0;
+
+    message.channel.send({embed:{
+      
+    
+
+
+}
