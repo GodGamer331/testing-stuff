495d27ccb7e988ced88bf1c2f5f3c641b0df4240
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-27 16:53:12 +0100

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -8,8 +8,30 @@ module.exports.run = async (bot, message, args) => {
     if (bal === null) bal = 0;
 
     message.channel.send({embed:{
+        color: 0x42f48c,
+        fields: [
+            {
+                name: "Account Owner",
+                value: message.author.username,
+               
+            },
+            {
+                name: "Account Balance:",
+                value: bal,
+            },
+        ],
+        author: {
+            icon_url: message.author.avatarURL,
+            text: "Bank Vault!"
+        }
+            
+    }})
       
     
 
 
+}
+module.exports.help = {
+    name: "bal",
+    aliases: []
 }
