b192c40669283f0af916238d2d948a56283b31b5
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-27 16:37:48 +0100

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -6,21 +6,21 @@ exports.run = async (client, message, args, config) => {
 
     let author = db.fetch(`money_${message.author.id}`)
 
-    if (args[0] == 'moderator') {
-        if (!message.guild.roles.find("name", 'Moderator')) return message.channel.send('I could not find a role by the name of `Moderator`, check with the owners.')
-        if (author < 700) return message.channel.send('You need atleast `700$` to purchase the moderator role.') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
+    if (args[0] == 'banana') {
+        if (!message.guild.roles.find("name", 'BANANA')) return message.channel.send('I could not find a role by the name of `BANANA`, check with the owners.')
+        if (author < 50) return message.channel.send('You need atleast `50$` to purchase the moderator role.') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
         
-        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", 'Moderator'))
+        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", 'BANANA'))
 
-        db.subtract(`money_${message.author.id}`, 700)
-        message.channel.send(message.author.tag + ' You successfully bought the Moderator role for `700$`')
-    } else if(args[0] == 'admin') {
-        if (!message.guild.roles.find("name", 'Admin')) return message.channel.send('I could not find a role by the name of `Admin`, check with the owners.')
-        if (author < 1800) return message.channel.send('You need atleast `1800$` to purchase the admin role.') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
-        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", 'Admin')) // get the role & add it
+        db.subtract(`money_${message.author.id}`, 50)
+        message.channel.send(message.author.tag + ' You successfully bought the BANANA role for `50$`')
+    } else if(args[0] == 'cocacola') {
+        if (!message.guild.roles.find("name", 'Coca-Cola')) return message.channel.send('I could not find a role by the name of `Coca-Cola`, check with the owners.')
+        if (author < 200) return message.channel.send('You need atleast `200$` to purchase Coca-Cola.') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
+        message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", 'Coca-Cola')) // get the role & add it
 
-        db.subtract(`money_${message.author.id}`, 1800)
-        message.channel.send(message.author.tag + ' You successfully bought the Admin role for `1800$`')
+        db.subtract(`money_${message.author.id}`, 200)
+        message.channel.send(message.author.tag + ' You successfully bought the Coca-Cola role for `200$`')
     }
 
 
