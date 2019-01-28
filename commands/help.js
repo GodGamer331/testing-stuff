851433e7213f0bc0a64eb1eda40ec8d2b4e832e0
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-28 22:28:29 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -127,7 +127,7 @@ module.exports.run = async (bot, message, args) => {
                     },
               }})};
            return;
-};
+
                 
                  
                    
