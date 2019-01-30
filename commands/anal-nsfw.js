80efa830ea7b4a8d7318ab80b66d90552814e8ff
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-31 00:17:28 +0100

    Create anal-nsfw.js

---
+++ commands/anal-nsfw.js
@@ -0,0 +1,30 @@
+const randomPuppy = require('random-puppy'); 
+
+module.exports.run = async (bot, message, args) => {
+ 
+ if(!message.channel.nsfw) return message.reply("Please use this command in channels that are marked as NSFW!");
+ let reddit = [
+               "analGif",
+               "anal",
+               "sexInAss"
+  ]
+
+let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
+
+ message.channel.startTyping(); 
+
+randomPuppy(subreddit).then(async url => {
+                    await message.channel.send({
+                            files: [{ 
+                                  attachment: url, 
+                                  name: 'lesbian!.png' 
+                          }]           
+                   }).then(() => message.channel.stopTyping()); 
+    }).catch(err => console.error(err)); 
+
+};
+
+ module.exports.help = {
+           name: 'anal', 
+           aliases: ['tits']
+} 
