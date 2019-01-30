53bd4f62e6131c186a647860aa098891aa228016
lepax91 <44847340+lepax91@users.noreply.github.com>
2019-01-30 14:50:31 +0100

    Create test.js

---
+++ commands/NSFW/test.js
@@ -0,0 +1,33 @@
+const randomPuppy = require('random-puppy'); 
+
+module.exports.run = async (bot, message, args) => {
+ 
+ if(!message.channel.nsfw) return message.reply("Please use this command in channels that are marked as NSFW!");
+ let reddit = ["boobs", 
+               "tits",
+               "animeboobs",
+               "animetits"
+                       
+                      
+                       
+  ]
+
+let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
+
+ message.channel.startTyping(); 
+
+randomPuppy(subreddit).then(async url => {
+                    await message.channel.send({
+                            files: [{ 
+                                  attachment: url, 
+                                  name: 'boobs.png' 
+                          }]           
+                   }).then(() => message.channel.stopTyping()); 
+    }).catch(err => console.error(err)); 
+
+};
+
+ module.exports.help = {
+           name: 'test', 
+           aliases: ['tits']
+} 
