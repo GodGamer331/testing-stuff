02f078e38e8e01f2ce0222e1d544630fed83519c
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-31 15:14:55 +0100

    Create gif-nsfw.js

---
+++ commands/gif-nsfw.js
@@ -0,0 +1,18 @@
+const discord = require('discord.js');
+const superagent = require('superagent')
+
+exports.run = (client, msg, args) => {
+  if (msg.channel.nsfw === true) {
+    superagent.get('https://nekobot.xyz/api/image')
+    .query({ type: 'pgif'})
+    .end((err, response) => {
+      msg.channel.send({ file: response.body.message });
+    });
+  } else {
+    msg.channel.send("This isn't NSFW channel!")
+  }
+};
+module.exports.help = {
+           name: 'pgif', 
+           aliases: ['tits']
+} 
