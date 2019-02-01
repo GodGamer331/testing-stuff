fbbe68d070e40d4b285d2e32e64a75203b7fab59
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-01 15:20:24 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -27,7 +27,7 @@ module.exports.run = async (bot, message, args) => {
             .setColor("GREEN");
             message.channel.send(embed)
             //if (args[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
-            else message.channel.send("Sorry, Please ask another question.");
+           // else message.channel.send("Sorry, Please ask another question.");
 }
 
 module.exports.conf = {
