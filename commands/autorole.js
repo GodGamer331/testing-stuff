03134533d07985bd1ced2a7b2c8c4b2ff7746ef0
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-02 17:48:45 +0100

    Update autorole.js

--- commands/autorole.js
+++ commands/autorole.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args, func) => {
    if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("You do not have enought permissions to do that! (Administrator)")
    if(!args.join(" ")) return message.reply("Please specify a role. Example: ``!=set-autorole Member``")
    
-   db.setText(`autorole_${message.guild.id}`, args.join(" ").trim()).then(i => {
+   db.set(`autorole_${message.guild.id}`, args.join(" ").trim()).then (i => {
    
        message.channel.send("Succesfully changed autorole to: " + i.text);
    
