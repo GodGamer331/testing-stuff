a7b9b77046189e2d896f0c15f62e2f3088aca165
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-02 17:38:33 +0100

    Create autorole.js

---
+++ commands/autorole.js
@@ -0,0 +1,17 @@
+const db= require('quick.db');
+
+module.exports.run = async (bot, message, args, func) => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("You do not have enought permissions to do that! (Administrator)")
+   if(!args.join(" ")) return message.reply("Please specify a role. Example: ``!=set-autorole Member``")
+   
+   db.updateText(`autorole_${message.guild.id}`, args.join(" ").trim()).then(i => {
+   
+       message.channel.send("Succesfully changed autorole to: " + i.text);
+   
+   })
+}
+
+module.exports.help = {
+    name: "set-autorole",
+    aliases: []
+}
