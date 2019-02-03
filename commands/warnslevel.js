8f004f496413ed46939200c70e347ac1083e5b1d
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-03 02:19:04 +0100

    Update warnslevel.js

--- commands/warnslevel.js
+++ commands/warnslevel.js
@@ -4,13 +4,15 @@ const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
-
+  
+  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]) || message.author
+  
   if(!warns[wUser.id]) warns[wUser.id] = {
     warns: 0
   };
   
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
-  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]) || message.author
+ // let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]) || message.author
   if(!wUser) return message.reply("Couldn't find them yo");
   let warnlevel = warns[wUser.id].warns;
 
