54c042798b753a8825884298b0481ddc921d1908
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-03 02:13:17 +0100

    Update testwarn.js

--- commands/testwarn.js
+++ commands/testwarn.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("No can do pal!");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("Couldn't find them yo");
-  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
+  //if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
   let reason = args.join(" ").slice(22);
 
   if(!warns[wUser.id]) warns[wUser.id] = {
