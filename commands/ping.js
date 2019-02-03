271123c777e0e09c8966c8011fd97aa7e10c980f
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-03 01:29:15 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js")
 exports.run = async (client, message, args, color) => {
 
 
-    let diff = (Date.now());
+    //let diff = (Date.now());
     
     let API = (client.ping).toFixed(2)
         
@@ -13,6 +13,7 @@ exports.run = async (client, message, args, color) => {
         .setColor("RANDOM")
         .addField("📶 Latency", `${diff}ms`, true)
         .addField("💻 API", `${API}ms`, true)
+        .setTimestamp()
         .setFooter("Developer: JustNela#8752");
         message.edit(embed);
       
