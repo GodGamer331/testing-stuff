690852257c0e3a0a80da98a79411109bdc3ae51c
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-04 00:30:08 +0100

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -37,7 +37,7 @@ module.exports.run = async (bot, message, args) => {
             //let question = args.join(" ").slice(22);
             var embed = new Discord.RichEmbed()
             .setTitle("Magic 8Ball!")
-            .setDescription("You flipped coin! " + fortunes[Math.floor(Math.random() * fortunes.length)] + " have dorpped!")
+            .setDescription("You rolled " + ftunes[Math.floor(Math.random() * fortunes.length)])
            // .addField("Question:", `${question}`)
            // .addField("Answer:", `${fortunes[Math.floor(Math.random() * fortunes.length)]}`)
             .setColor("GREEN");
