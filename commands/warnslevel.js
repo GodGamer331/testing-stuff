18f877a8e4c3e764a0f679752286f2ad0177c54a
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-03 03:02:53 +0100

    Update warnslevel.js

--- commands/warnslevel.js
+++ commands/warnslevel.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
+let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  
 let warns = JSON.parse(fs.readFileSync("./Storage/warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
   
-  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   
   if(!warns[wUser.id]) warns[wUser.id] = {
     warns: 0
