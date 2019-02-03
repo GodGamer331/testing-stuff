e040357b67bb889539949b53650a26cd03386758
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-03 22:01:26 +0100

    Update testwarn.js

--- commands/testwarn.js
+++ commands/testwarn.js
@@ -2,7 +2,7 @@ const warns = require("../data/warns.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 exports.run = function (bot, message, args) {
-  let perms = bot.elevation(message)
+//  let perms = bot.elevation(message)
   let pid = args.slice(0).join(" ");
   if(pid.length < 1) return message.reply("Please provide a warning ID to remove.")
   let list = Object.keys(warns);
@@ -16,7 +16,7 @@ exports.run = function (bot, message, args) {
   }
 
   if(!found) return message.reply("No warning found with that warning ID.")
-  if(perms < 20 && warns[found].server.id !== message.guild.id) return message.reply("You can not do this as this warning was not issued in this server.")
+  //if(perms < 20 && warns[found].server.id !== message.guild.id) return message.reply("You can not do this as this warning was not issued in this server.")
   message.channel.send(`Deleting the case of ${warns[found].user.name}\nReason: ${warns[found].reason}\nServer: ${warns[found].server.name}`)
   delete warns[found]
   fs.writeFile("./data/warns.json", JSON.stringify(warns))
