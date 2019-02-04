e38be580071edeacb5fa6a2d2b0127c8470ae497
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-04 16:37:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -151,6 +151,19 @@ bot.on("message", async message => {
         
   }
   }
+  if(cmd === `${prefix}addmoney` || cmd === `${prefix}moneyadd` || cmd === `${prefix}add`) {
+  if (!message.member.hasPermission('ADMINISTRATOR')) {
+        return message.reply('You do not have enough permission to use this command.')
+    }
+
+    if (!args[0]) return message.reply('Please specify an amount to add.')
+    if (isNaN(args[0])) return message.reply('That was not a valid number!')
+
+    let user = message.mentions.users.first() || message.author
+    message.channel.send('Successfully added ' + args[0] + ' to ' + user)
+    db.add(money_${user.id}, args[0])
+
+  }
 });
         
   
