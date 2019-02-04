41ce13b5bc55c6db2ffd0d4a5f9763d3b605adba
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-04 01:22:53 +0100

    Create trollkick.js

---
+++ commands/trollkick.js
@@ -0,0 +1,16 @@
+const Botconfig = require("../botconfig.json");
+const Discord = require("discord.js");
+//const ms = require("ms")
+
+module.exports.run = async (bot, message, args) => {
+   var embed = new Discord.RichEmbed()
+   .setAuthor("You got trolled!")
+   .setColor("RED")
+   .setDescription("Lol you really thinked I misspelled that? No I didnt :D \n You cant kick anyone!");
+   message.chsnnel.send(embed)
+}
+
+module.exports.help = {
+    name: "kik",
+    aliases: []
+}
