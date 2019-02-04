3f32fa79613fd542a2e55fab1da17c3e34d1eb33
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-04 01:38:31 +0100

    Update trollkick.js

--- commands/trollkick.js
+++ commands/trollkick.js
@@ -1,8 +1,10 @@
 const Botconfig = require("../botconfig.json");
 const Discord = require("discord.js");
 //const ms = require("ms")
+const bot = new Discord.Client({disableEveryone: true});
 
-module.exports.run = async (bot, message, args) => {
+//module.exports.run = async (bot, message, args) => {
+bot.on("message", async message => {
    if (message.content === '!=test' || message.content === '!=t'){
    var embed = new Discord.RichEmbed()
    .setAuthor("You got trolled!")
