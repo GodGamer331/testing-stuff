771861e9d209a5485857e8bba43623a8fbe15593
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-06 18:11:07 +0100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,10 +1,10 @@
 exports.run = (client, message) => {
 	let prefix = message.content.split(" ")[1];
-	if(!prefix) return system.shortsend(message.channel, `Try with : \`$$prefix [the prefix you want]\``);
-	if(prefix.length > 20) return system.shortsend(message.channel, `Prefix can't contain more than 20 letters.`);
+	if(!prefix) return console.shortsend(message.channel, `Try with : \`$$prefix [the prefix you want]\``);
+	if(prefix.length > 20) return console.shortsend(message.channel, `Prefix can't contain more than 20 letters.`);
 
 	client.mysql.query(`SELECT * FROM guilds WHERE g_id = ${message.guild.id}`, (err, rows) => {
-		if(err) return system.log(err, "error");
+		if(err) return console.log(err, "error");
 
 		client.mysql.query(`UPDATE guilds SET prefix = "${prefix}" WHERE g_id = ${message.guild.id}`);
 		message.channel.send({
