cd38a29a3bf6fa5b2f00b9c4b47a98dfb4c684ad
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-02-06 22:10:02 +0100

    Create verify-key.js

---
+++ commands/verify-key.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const PREFIX = "!="
+
+// This is the brackets in which the command goes in
+module.exports.run = async (bot, message, args) => {
+    if (!message.content.startsWith(PREFIX)) return;
+      message.delete(1)
+//why I cant make this help to work???????????????? IDK CUZ ITS RETARDET!!!
+      if(args[0] == "Gfidts6653HsixVs_jeiHeb"){
+        message.reply("please check your dms");
+            message.author.send({embed: {
+                  color: RANDOM,
+                  thumbnail: {
+                      url: (message.author.displayAvatarURL)
+                    },
+                  fields: [
+                    {
+                        name: "Verified!",
+                        value: "This key was redeemed successfully! This key was for: VIP",
+                    },
+
+              ],
+                  timestamp: new Date(),
+                  footer: {
+                    icon_url: bot.user.displayAvatarURL ,
+                    text: "GodBot™ | Made by JustNela#8752 | v3.4-Alpha",
+                  },
+                  author: {
+                      icon_url: message.guild.iconURL,
+                      name: "GodBot verification key!",
+                    }
+              }});
+            return;
+          
+        };
+module.exports.help = {
+  name: "verify-key"
+}
